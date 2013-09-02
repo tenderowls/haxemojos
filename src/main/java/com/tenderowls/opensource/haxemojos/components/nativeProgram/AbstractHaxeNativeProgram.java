6c8e49aefd1df465dd2d98ca5bde069a12f990be
Dukobpa3 <a.burdun@gmail.com>
2013-09-02 18:55:29 +0300

    upd

---
+++ src/main/java/com/tenderowls/opensource/haxemojos/components/nativeProgram/AbstractHaxeNativeProgram.java
@@ -0,0 +1,11 @@
+package com.tenderowls.opensource.haxemojos.components.nativeProgram;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dukobpa3
+ * Date: 02.09.13
+ * Time: 18:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstractHaxeNativeProgram {
+}
